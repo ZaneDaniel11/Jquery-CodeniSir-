a874db12af6de942eae7931bbb57ef27113a7b54
Zane Daniel <118499857+yawaka21@users.noreply.github.com>
2024-03-02 10:36:21 +0800

    Add files via upload

---
+++ abcd/main.js
@@ -0,0 +1,43 @@
+function loadData(){
+$.ajax({
+    url: "categories.php"
+}).done(function(data){
+console.log(data);
+
+let result = JSON.parse(data);
+
+var template = document.querySelector("#categoryRowTemplate");
+var parent = document.querySelector("#tableBody");
+
+result.forEach(item => {
+let clone = template.content.cloneNode(true);
+clone.querySelector("tr td.tdID").innerHTML = item.category_id;
+clone.querySelector("tr td.tdName").innerHTML = item.category_name;
+parent.appendChild(clone);
+});
+});
+}
+loadData();
+
+$("#btnSaveCategory").click(function(){
+var categoryName = document.querySelector("#categoryName").value;
+if(categoryName.length > 0){
+    $.ajax({
+        url:  "categories.create.php",
+        type: "GET",
+        data: {
+            name: categoryName
+        }
+    }).done(function(data){
+        let result = JSON.parse(data);
+        if(result.res == "success"){
+           
+            loadData();
+             //to hide modal
+             $("#exampleModal").modal("toggle"); 
+             //clear form
+             document.querySelector("form").reset();
+        }
+    })
+}
+});
---
+++ category_table.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2024 at 03:36 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `new_store_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_table`
+--
+
+CREATE TABLE `category_table` (
+  `category_id` int(50) NOT NULL,
+  `category_name` varchar(50) NOT NULL,
+  `date_created` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category_table`
+--
+
+INSERT INTO `category_table` (`category_id`, `category_name`, `date_created`) VALUES
+(1, '212', '2024-03-02 02:33:51.648634'),
+(2, 'tes', '2024-03-02 02:34:04.851171'),
+(3, 'testing', '2024-03-02 02:34:11.888919');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category_table`
+--
+ALTER TABLE `category_table`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category_table`
+--
+ALTER TABLE `category_table`
+  MODIFY `category_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
